f4b9f6e90115f8a64eacb1c520e209ba848207f3
WilliamLemosJR <127670993+WilliamLemosJR@users.noreply.github.com>
2023-10-02 14:48:33 -0300

    criando exercicio de angular

--- segundo-projeto/src/app/app.module.ts
+++ segundo-projeto/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponet } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponet,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
