4d3c6f718e81f3bb7841b894d55ed9c7f4b02988
WilliamLemosJR <127670993+WilliamLemosJR@users.noreply.github.com>
2023-10-04 08:00:34 -0300

    estudando serviços

--- segundo-projeto/src/app/cursos/cursos.component.ts
+++ segundo-projeto/src/app/cursos/cursos.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { CursosService } from './cursos.service';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
@@ -8,14 +10,18 @@ import { Component } from '@angular/core';
 export class CursosComponent {
   nomeGithub: string ;
 
-  cursos : string [] =['HTML', 'CSS' , 'JavaScript']
+  cursos : string [];
 
-  constructor () {
+  constructor (private cursosService:CursosService) {
     this.nomeGithub = 'https://github.com/WilliamLemosJR'
 
-    for (let i=0; i<this.cursos.length; i++){
+   /* for (let i=0; i<this.cursos.length; i++){
       let curso = this.cursos[i] ;
-    }
+    }*/
+
+    //var servico = new CursosService();
+
+    this.cursos =this.cursosService.getCursos();
   }
   
 }
