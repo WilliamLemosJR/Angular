552a3078f267eb1f8c426dbd4aaf81fad6c40e83
WilliamLemosJR <127670993+WilliamLemosJR@users.noreply.github.com>
2023-10-10 17:48:35 -0300

    ciclo do component

---
+++ data-binding/src/app/ciclo/ciclo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo',
+  templateUrl: './ciclo.component.html',
+  styleUrls: ['./ciclo.component.css']
+})
+export class CicloComponent {
+
+  valorInicial: number =10
+
+}
