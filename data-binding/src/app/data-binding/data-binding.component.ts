bf85cff6eedc162d66a0d85d981ded2ca745ae68
WilliamLemosJR <127670993+WilliamLemosJR@users.noreply.github.com>
2023-10-10 11:22:23 -0300

    outputs

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -28,6 +28,8 @@ export class DataBindingComponent {
 
   nomeDoCurso: string = 'Angular'
 
+  valorInicial=15
+
 
 
   getValor(){
@@ -52,6 +54,10 @@ export class DataBindingComponent {
 
   onMouseOverOut(){
     this.isMouseOver =!this.isMouseOver
+  }
+
+  onMudouValor(evento: any){
+    console.log(evento.novoValor)
   }
   
 }
--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,Input, EventEmitter,Output } from '@angular/core';
 
 @Component({
-  selector: 'app-output-property',
+  selector: 'contador',
   templateUrl: './output-property.component.html',
   styleUrls: ['./output-property.component.css']
 })
 export class OutputPropertyComponent {
+
+  @Input() valor:number =0
+
+  @Output() mudoValor= new EventEmitter()
+
+  incrementa(){
+    this.valor++
+    this.mudoValor.emit({novoValor: this.valor})
+  }
+
+  decrementa(){
+    this.valor--
+    this.mudoValor.emit({novoValor: this.valor})
+  }
 
 }
